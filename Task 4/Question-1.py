47d459ed146bccf8ee952231cfedfab54cbfd5db
VishnuKasilya25 <you@example.com>
2022-02-22 10:48:59 +0530

    PY

---
+++ Task 4/Question-1.py
@@ -0,0 +1,16 @@
+#Question-1
+
+# This program adds two numbers
+
+num1 = int(input("Enter First number : "))
+num2 = int(input("Enter Second number : "))
+
+# Add two numbers
+
+sum = num1 + num2
+
+# Display the sum
+
+print(num1," + ",num2," = ",sum)
+print("Sum=", sum)
+
